4b020c860064b92be9949a635e7a2b1711150ac9
Branislav Trstenský <bt7s7k7@hotmail.sk>
2026-01-23 21:39:51 +0100

    Fixed crash in newer Node versions

--- builder.cjs
+++ builder.cjs
@@ -4,7 +4,7 @@ const { readFileSync, readdirSync } = require("node:fs")
 const { join, relative } = require("node:path")
 
 module.exports = {
-    modifyOptions(options) {
+    modifyOptions(/** @type {import("esbuild").BuildOptions} */ options) {
         (options.plugins ??= []).push({
             name: "server-files",
             setup(build) {
@@ -19,6 +19,7 @@ module.exports = {
                 })
 
                 build.onLoad({ filter: /.*/, namespace: "server-files-ns" }, () => {
+                    /** @type {Record<string, string>} */
                     const result = {
                         "quick-front.js": readFileSync(join(__dirname, "node_modules/quick-front/dist/assets/quick-front.js"), "utf-8"),
                         "quick-front.css": readFileSync(join(__dirname, "node_modules/quick-front/dist/assets/main.css"), "utf-8"),
@@ -26,7 +27,7 @@ module.exports = {
 
                     for (const entry of readdirSync(join(__dirname, "public"), { withFileTypes: true, recursive: true })) {
                         if (!entry.isFile()) continue
-                        const fullPath = join(entry.path, entry.name)
+                        const fullPath = join(entry.path ?? entry.parentPath, entry.name)
                         const relativePath = relative(join(__dirname, "public"), fullPath)
                         result[relativePath] = readFileSync(fullPath, "utf-8")
                     }
